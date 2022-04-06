5eeabbee8f4dc98fd90cb0c6db92dd757aa665ee
Giovanni Montoya <giovanni87070665@hotmail.com>
2022-04-06 13:52:59 -0500

    Se tiene el proyecto de portafolio trabajando con Webpack

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import Template from './templates/Template.js';
-console.log('hola');
-
+import Template from '@templates/Template.js';
+import '@styles/main.css';  //Añadir los estilos css
+import '@styles/vars.styl';
+console.log("Hola desde webpack.config.dev.js");
 (async function App() {
   const main = null || document.getElementById('main');
   main.innerHTML = await Template();
 })();
+
