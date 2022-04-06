ba593247300ed3fa1b8cd835b08926197261b732
Giovanni Montoya <giovanni87070665@hotmail.com>
2022-04-06 16:23:56 -0500

    Fix Bug

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Template from '@templates/Template.js';
 import '@styles/main.css';  //Añadir los estilos css
 import '@styles/vars.styl';
-console.log("Hola desde webpack.config.dev.js");
+
 (async function App() {
   const main = null || document.getElementById('main');
   main.innerHTML = await Template();
